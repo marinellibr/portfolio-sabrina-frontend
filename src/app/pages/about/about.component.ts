1cb56eae679ed38587049e1cd90c059298eef5a7
Luiz Henrique Marinelli <luizh0508@gmail.com>
2026-06-22 21:41:01 -0300

    feat: enhance About and Contact pages with translations and improved layout; remove unused CSS

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
+import { TranslatePipe } from "@ngx-translate/core";
 
 @Component({
   selector: "app-about",
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: "./about.component.html",
   styleUrl: "./about.component.scss",
 })
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-contact',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  selector: "app-contact",
+  imports: [CommonModule, TranslatePipe],
+  templateUrl: "./contact.component.html",
+  styleUrl: "./contact.component.scss",
 })
 export class ContactComponent {
+  rawUrl =
+    "https://raw.githubusercontent.com/marinellibr/portfolio-sabrina-resources/8515e4debbfd5e4afeb106becac78dcdb0ca52f7/images/";
 
+  constructor(private translateService: TranslateService) {}
+
+  contacts: any[] = [
+    {
+      icon: "mail",
+      labelKey: "CONTACT.EMAIL_LABEL",
+      valueKey: "CONTACT.EMAIL_VALUE",
+      value: "sabrina21cardoso@gmail.com",
+    },
+    {
+      icon: "linkedin",
+      labelKey: "CONTACT.LINKEDIN_LABEL",
+      valueKey: "CONTACT.LINKEDIN_VALUE",
+      value: "linkedin.com/in/sabrinascardoso",
+    },
+    {
+      icon: "zap",
+      labelKey: "CONTACT.WHATSAPP_LABEL",
+      valueKey: "CONTACT.WHATSAPP_VALUE",
+      value: "+55 (11) 98939-8426",
+    },
+    {
+      icon: "work",
+      labelKey: "CONTACT.CV_LABEL",
+      valueKey: "CONTACT.CV_VALUE",
+      value: "baixe em pdf (:)",
+    },
+    {
+      icon: "behance",
+      labelKey: "CONTACT.BEHANCE_LABEL",
+      valueKey: "CONTACT.BEHANCE_VALUE",
+      value: "behance.net/sabrinacardoso7",
+    },
+  ];
+
+  handleContactClick(contact: any): void {
+    switch (contact.icon) {
+      case "mail":
+        window.location.href = `mailto:${contact.value}`;
+        break;
+      case "zap":
+        const whatsappNumber = contact.value.replace(/\D/g, "");
+        const message = this.translateService.instant(
+          "CONTACT.WHATSAPP_MESSAGE",
+        );
+        const encodedMessage = encodeURIComponent(message);
+        window.open(
+          `https://wa.me/${whatsappNumber}?text=${encodedMessage}`,
+          "_blank",
+        );
+        break;
+      case "work":
+        console.log("CV clicado");
+        break;
+      case "linkedin":
+      case "behance":
+        window.open(`https://${contact.value}`, "_blank");
+        break;
+    }
+  }
 }
